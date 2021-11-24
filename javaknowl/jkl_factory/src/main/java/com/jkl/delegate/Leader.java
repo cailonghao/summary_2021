a24e545e4694abbbd93b7d9ab428e1b8e135b82c
大白鲨 <cailonghao1234@foxmail.com>
2021-11-24 14:18:53 +0800

    no message

--- javaknowl/jkl_factory/src/main/java/com/jkl/delegate/Leader.java
+++ javaknowl/jkl_factory/src/main/java/com/jkl/delegate/Leader.java
@@ -8,5 +8,15 @@ import java.util.Map;
  */
 public class Leader {
 
-    private Map<String,IEmployee> map = new HashMap<String, IEmployee>();
+    private Map<String, IEmployee> targets = new HashMap<String, IEmployee>();
+
+
+    public Leader() {
+        targets.put("加密", new EmployeeA());
+        targets.put("登录", new EmployeeB());
+    }
+
+    public void doing(String commd) {
+        targets.get(commd).doing();
+    }
 }
