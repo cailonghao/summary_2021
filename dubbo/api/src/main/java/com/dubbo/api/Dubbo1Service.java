dcca28031d5c21005f2b59b69b1cd092b0ac1a39
longhaocai <cailonghao1234@gmail.com>
2021-12-01 20:17:45 +0800

    no message

---
+++ dubbo/api/src/main/java/com/dubbo/api/Dubbo1Service.java
@@ -0,0 +1,7 @@
+package com.dubbo.api;
+
+
+public interface Dubbo1Service {
+
+    String sayHello(String name);
+}
---
+++ springboot-example/example-hello/src/main/java/com/example/hello/HelloApplication.java
@@ -0,0 +1,23 @@
+package com.example.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * AutoConfiguration
+ * Starter
+ * Actuator
+ * springboot CLI
+ */
+@SpringBootApplication
+public class HelloApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(HelloApplication.class, args);
+        String[] defNames = context.getBeanDefinitionNames();
+        for (String str : defNames) {
+            System.out.println(str);
+        }
+    }
+}
